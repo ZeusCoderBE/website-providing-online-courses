f74197000b3814d339a8dc05598714acc832ef42
TrieuPhi <dtptrieuphidtp@gmail.com>
2023-10-18 16:27:43 +0700

    some files

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/LoginController.java
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/LoginController.java
@@ -8,10 +8,6 @@ public class LoginController {
 	@RequestMapping("/")
 	public String index()
 	{
-<<<<<<< HEAD
-		return "SignIn";
-=======
-		return "index";
->>>>>>> 010eddb3d6918022371de374b1d7b8c89b991f8b
+		return "DangKy";
 	}
 }
