853b49d70e0aa6c70e8d54d3482944cb83e4ae07
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-20 13:55:11 +0700

    handle conflict

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/Test.java
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/Test.java
@@ -8,11 +8,7 @@ public class Test {
 	@RequestMapping(value={"/test","/trangchu"})
 	public String index()
 	{
-<<<<<<< HEAD:ProjectCuoiKiWeb/src/main/java/vn/iotstar/Test.java
-		return "";
-=======
 		return "DangKy";
->>>>>>> 54463c1ba80401c925cb9a123a3c306d7e1c4f59:ProjectCuoiKiWeb/src/main/java/vn/iotstar/LoginController.java
 	}
 	
 }
