c9d7d057c74671a7b5867864bdc1e3425cbe583c
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-25 19:23:14 +0700

    Update

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/model/BaiHocDao.java
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/model/BaiHocDao.java
@@ -11,7 +11,7 @@ public class BaiHocDao {
 	DataBaseConnection dbC = new DataBaseConnection();
 
 	public List<BaiHoc> GetScience(BaiHoc baihoc) throws SQLException, ClassNotFoundException {
-		ResultSet rs = dbC.ExecuteQuery("exec sp_XemDanhSachKH " + baihoc.getMabaihoc() + "");
+		ResultSet rs = dbC.ExecuteQuery("sp_XemDanhSachBaiHoc " + baihoc.getMabaihoc() + "");
 		List<BaiHoc> listbaihoc = new ArrayList<BaiHoc>();
 		while (rs.next() == true) {
 			baihoc = new BaiHoc(rs.getInt("MaBaiHoc"), rs.getNString("TenBaiHoc"), rs.getDouble("ThoiGianHoanThanh"),
