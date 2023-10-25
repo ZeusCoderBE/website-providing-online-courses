eea03a49ca4c68b4500d542b513b0bfa4eae3194
Trần Văn Luân <tranluan0794@gmail.com>
2023-10-26 00:03:20 +0700

    design form create course

--- ProjectCuoiKiWeb/src/main/webapp/JavaScript/script.js
+++ ProjectCuoiKiWeb/src/main/webapp/JavaScript/script.js
@@ -1,3 +1,23 @@
+tinymce.init({
+    selector: 'textarea#default',
+    width: 1100,
+    height: 300,
+    plugins:[
+        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'prewiew', 'anchor', 'pagebreak',
+        'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media', 
+        'table', 'emoticons', 'template', 'codesample'
+    ],
+    toolbar: 'undo redo | styles | bold italic underline | alignleft aligncenter alignright alignjustify |' + 
+    'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+    'forecolor backcolor emoticons',
+    menu: {
+        favs: {title: 'menu', items: 'code visualaid | searchreplace | emoticons'}
+    },
+    menubar: 'favs file edit view insert format tools table',
+    content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px}'
+});
+
+
 function handleFocus() {
     const page = document.querySelector('.page');
     const sidebarHome = document.querySelector('.sidebar_home');
