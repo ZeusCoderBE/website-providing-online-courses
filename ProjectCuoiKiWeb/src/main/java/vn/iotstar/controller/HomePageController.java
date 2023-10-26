4e864012b04d70aa5793f38809e2e43379f56cd0
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-26 20:25:20 +0700

    Update

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/controller/HomePageController.java
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/controller/HomePageController.java
@@ -5,7 +5,6 @@ import vn.iotstar.model.*;
 import java.sql.SQLException;
 import java.util.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
