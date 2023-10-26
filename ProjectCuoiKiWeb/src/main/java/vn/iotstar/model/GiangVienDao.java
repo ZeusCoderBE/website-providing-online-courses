967d448705f654df5eda773ebc6cb47fbd33f7ef
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-26 20:19:03 +0700

    Update PJ

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/model/GiangVienDao.java
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/model/GiangVienDao.java
@@ -6,12 +6,7 @@ public class GiangVienDao {
 	DataBaseConnection dbC = new DataBaseConnection();
 
 	public int InsertGiangVien() {
-		String sql = "declare @manguoidung int\r\n"
-				+ "set @manguoidung=(SELECT  Top 1 MaNguoiDung FROM NGUOIDUNG\r\n"
-				+ "ORDER BY MaNguoiDung desc)\r\n"
-				+ "\r\n"
-				+ "insert into GIANGVIEN(MaGiangVien)\r\n"
-				+ "values(@manguoidung)";
+		String sql = "sp_TimTaiKhoanGiangVien";
 		int ketqua = dbC.ExecuteCommand(sql);
 		return ketqua;
 	}
