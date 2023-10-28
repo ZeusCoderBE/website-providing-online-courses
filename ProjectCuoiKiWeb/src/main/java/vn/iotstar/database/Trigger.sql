b36a2c28f683122ba246b00e495dcdbbd0c7ab48
Narius2030 <buiducnhan15122003@gmail.com>
2023-10-28 07:22:51 +0700

    Commit some files

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/database/Trigger.sql
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/database/Trigger.sql
@@ -2,6 +2,8 @@ CREATE OR ALTER TRIGGER tr_themDangKy ON THANHTOAN
 AFTER INSERT
 AS
 DECLARE @mand INT, @makh INT
+SELECT @mand=i.MaNguoiDung, @makh=i.MaKhoaHoc
+FROM inserted i
 BEGIN
 	INSERT INTO DANGKY VALUES(@mand, @makh)
 END
