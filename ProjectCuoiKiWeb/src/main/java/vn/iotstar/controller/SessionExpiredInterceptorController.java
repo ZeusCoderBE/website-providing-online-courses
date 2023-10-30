aab8447d31d7fe4708f6ab27c1079c0228e95329
Dang Nguyen Quang Huy <hdang1696@gmail.com>
2023-10-30 20:58:47 +0700

    Update Gmail

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/controller/SessionExpiredInterceptorController.java
+++
@@ -1,8 +0,0 @@
-package vn.iotstar.controller;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.function.support.HandlerFunctionAdapter;
-public class SessionExpiredInterceptorController  extends HandlerFunctionAdapter{
-
-}
