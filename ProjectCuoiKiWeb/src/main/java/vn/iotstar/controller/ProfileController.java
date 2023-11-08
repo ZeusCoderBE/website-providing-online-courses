abc742696d6e14029203c747f0f637bd59911c0d
Narius2030 <buiducnhan15122003@gmail.com>
2023-11-08 20:17:17 +0700

    Adding project

---
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/controller/ProfileController.java
@@ -0,0 +1,109 @@
+package vn.iotstar.controller;
+
+import vn.iotstar.model.*;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class ProfileController {
+	NguoiDungDao ndD = new NguoiDungDao();
+	HocVienDao hvD = new HocVienDao();
+	KhoaHocDao khD = new KhoaHocDao();
+	TheDao tD = new TheDao();
+
+	@RequestMapping(value = "myprofiles", method = RequestMethod.GET)
+	public String ShowProfile(HttpSession session, ModelMap model) throws ClassNotFoundException, SQLException {
+		HocVien hv = (HocVien) session.getAttribute("hocvien");
+		HocVien hocvien = new HocVien();
+		The the = tD.getAThe(hv.getManguoidung());
+		hocvien = ndD.TimThongTinDN(hv.getEmail());
+		if (hocvien.getEmail() != null) {
+			model.addAttribute("thongtin", hocvien);
+			model.addAttribute("the", the);
+		}
+		else {
+			hocvien = ndD.TimThongTinDN_Id(hv.getManguoidung());
+			model.addAttribute("thongtin", hocvien);
+		}
+		return "profile";
+	}
+
+	public void RealoadKhoaHoc(ModelMap model) throws ClassNotFoundException, SQLException {
+		List<KhoaHoc> ListKH = null;
+		ListKH = khD.GetListCourses();
+		model.addAttribute("danhsachkh", ListKH);
+	}
+
+	@RequestMapping(value = "changepass", method = RequestMethod.POST)
+	public String ChangePassWord(ModelMap model, HttpSession session,
+			@RequestParam(value = "password", required = false, defaultValue = "null") String password,
+			@RequestParam(value = "newpass", required = false, defaultValue = "null") String newpass,
+			@RequestParam(value = "repass", required = false, defaultValue = "null") String repass)
+			throws ClassNotFoundException, SQLException {
+		HocVien hv = (HocVien) session.getAttribute("hocvien");
+		HocVien hocvien = new HocVien();
+		hocvien = ndD.TimThongTinDN(hv.getEmail());
+		HocVien hovVien = new HocVien(newpass, hocvien.getManguoidung());
+		String url = "";
+		if (password.equals("null") || newpass.equals("null") || repass.equals("null")) {
+			session.setAttribute("thongtinsai", "Bạn Vui Lòng Nhập Theo Hướng Dẫn ở dưới nếu bạn muốn đổi mật khẩu");
+			url = "redirect:/myprofiles";
+		} else if (hocvien.getMatkhau().equals(newpass)) {
+			session.setAttribute("thongtinsai",
+					"Mật Khẩu này bạn đã dùng cho lần cập nhật trước rồi ! Vui Lòng Sử dụng mật khẩu khác");
+			url = "redirect:/myprofiles";
+		} else if (!hocvien.getMatkhau().equals(password)) {
+			session.setAttribute("thongtinsai", "Bạn Nhập Mật Khẩu Cũ Chưa Đúng");
+			url = "redirect:/myprofiles";
+		} else if (!newpass.equals(repass)) {
+			session.setAttribute("thongtinsai", "Bạn Nhập Xác Nhận Mật Khẩu Chưa Đúng");
+			url = "redirect:/myprofiles";
+		} else if (hvD.UpdateMatKhauHocVien(hovVien) == 1) {
+			RealoadKhoaHoc(model);
+			url = "SignIn";
+		} else {
+			session.setAttribute("thongtinsai", "Quá Trình Cập Nhật Thất Bại");
+			url = "redirect:/myprofiles";
+		}
+
+		return url;
+	}
+
+	@RequestMapping(value = "myprofile", method = RequestMethod.POST)
+	public String ChangeProfie(ModelMap model, HttpSession session, @RequestParam("username") String username,
+			@RequestParam("quocgia") String quocgia, @RequestParam("sdt") String sdt,
+			@RequestParam("trinhdo") String trinhdo, @RequestParam("diachi") String diachi,
+			@RequestParam("vungmien") String vungmien, @RequestParam("email") String email)
+			throws ClassNotFoundException, SQLException
+
+	{
+		String url = "";
+		HocVien hv = (HocVien) session.getAttribute("hocvien");
+		HocVien hocvien = new HocVien(hv.getManguoidung(), username, email, sdt, quocgia, vungmien, diachi, trinhdo, "",
+				"");
+		email = hocvien.getEmail();
+		try {
+			if (hvD.UpdateHocVien(hocvien) == 1) {
+				RealoadKhoaHoc(model);
+				model.addAttribute("thongtin", hocvien);
+				session.setAttribute("thongbaothanhcong", "Bạn đã thay đổi thông tin thành công");
+				url = "redirect:/homepages";
+			} else {
+				url = "redirect:/myprofiles";
+				throw new SQLException("Quá Trình Cập Nhật Bị Thất Bại Số Điện Thoại Đã tồn tại");
+			}
+		} catch (SQLException e) {
+			session.setAttribute("thongbao", e.getMessage());
+		}
+		return url;
+	}
+}
---
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/controller/SignUpController.java
@@ -0,0 +1,75 @@
+package vn.iotstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import vn.iotstar.model.*;
+
+@Controller
+public class SignUpController {
+	NguoiDungDao ndD = new NguoiDungDao();
+	HocVienDao hvD = new HocVienDao();
+	GiangVienDao gvD = new GiangVienDao();
+
+	@RequestMapping(value = "/dang-ky", method = RequestMethod.GET)
+	public String SignIn() {
+		return "DangKy";
+	}
+
+	@RequestMapping(value = "/dang-ky", method = RequestMethod.POST, params = "tkhocvien")
+	public String XuLiSignUpHocVien(Model map, @RequestParam("username") String username,
+			@RequestParam("email") String email, @RequestParam("password") String password,
+			@RequestParam("sdt") String sdt,
+			@RequestParam("confirmpassword") String confipass, @RequestParam("quocgia") String quocgia,
+			@RequestParam(value = "tkhocvien", required = false, defaultValue = "null") String tkhocvien) {
+		String url = "";
+		if ("null".equals(tkhocvien)) {
+			map.addAttribute("thongbaoloi", "Bạn Chưa Chọn Loại Tài Khoản !");
+		} else if (!password.equals(confipass)) {
+			map.addAttribute("xacnhanmksai", "Xác Nhận Mật Khẩu Không Đúng !");
+			url = "DangKy";
+		} else {
+			HocVien hv = new HocVien(username, email, quocgia, password,sdt);
+			if (ndD.SignUp(hv) == 1) {
+				hvD.InsertHocVien();
+				map.addAttribute("thongbaodung", " Chúc Mừng Bạn Đã Đăng Ký Thành Công !");
+				url = "SignIn";
+			} else {
+				map.addAttribute("thongbaodksai", "Email Và Số Điện Thoại Đã Tồn Tại !");
+				url = "DangKy";
+			}
+		}
+		return url;
+	}
+
+	@RequestMapping(value = "/dang-ky", method = RequestMethod.POST, params = "tkgiaovien")
+	public String XuLiSignUpGiaoVien(Model map, @RequestParam("username") String username,
+			@RequestParam("email") String email, @RequestParam("password") String password,
+			@RequestParam("confirmpassword") String confipass, @RequestParam("quocgia") String quocgia,
+			@RequestParam("sdt") String sdt,
+			@RequestParam(value = "tkgiaovien", required = false, defaultValue = "null") String tkgiaovien) {
+		String url = "";
+		if ("null".equals(tkgiaovien)) {
+			map.addAttribute("thongbaoloi", "Bạn Chưa Chọn Loại Tài Khoản !");
+		} else if (!password.equals(confipass)) {
+			map.addAttribute("xacnhanmksai", "Xác Nhận Mật Khẩu Không Đúng !");
+			url = "DangKy";
+		} else {
+			GiangVien gv = new GiangVien(username, email, quocgia, password,sdt);
+			int ketqua = ndD.SignUp(gv);
+			if (ketqua == 1) {
+				gvD.InsertGiangVien();
+				map.addAttribute("thongbaodkdung", "Chúc Mừng Bạn Đã Đăng Kí Thành Công !");
+				url = "SignIn";
+			} else {
+				map.addAttribute("thongbaodkgvsai", "Thất Bại !");
+				url = "DangKy";
+			}
+		}
+		return url;
+	}
+
+}
