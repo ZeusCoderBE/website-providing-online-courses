a6a255e90bdf2aa2cd630fa4fff25fcbf51d9a71
Trần Văn Luân <tranluan0794@gmail.com>
2023-11-10 11:29:09 +0700

    Updating css files

--- ProjectCuoiKiWeb/src/main/java/vn/iotstar/database/ViewDB.sql
+++ ProjectCuoiKiWeb/src/main/java/vn/iotstar/database/ViewDB.sql
@@ -4,12 +4,15 @@ select HOCVIEN.MaHocVien,NGUOIDUNG.HoTen,NGUOIDUNG.Email,NGUOIDUNG.Sdt,
 	NGUOIDUNG.QuocGia,NGUOIDUNG.VungMien,NGUOIDUNG.DiaChi,NGUOIDUNG.TrinhDo,
 	HOCVIEN.LoaiTaiKhoan,NguoiDung.MatKhau  From NGUOIDUNG join  HOCVIEN
 	on HOCVIEN.MaHocVien=NGUOIDUNG.MaNguoiDung
+
+	go
 -- Xem Giỏ Hàng
+
 Create Or Alter View v_XemGioHang
 as 
 select NGUOIDUNG.HoTen,NGUOIDUNG.Email,KHOAHOC.TenKhoaHoc,KHOAHOC.DanhGia,KHOAHOC.MaTacGia
 ,KHOAHOC.TrinhDoDauVao,KHOAHOC.GiaTien,KHOAHOC.ThoiGianHoanThanh,
-KHOAHOC.LinhVuc,KhoaHoc.NgayPhatHanh,NGUOIDUNG.MaNguoiDung From NGUOIDUNG
+KHOAHOC.LinhVuc,KhoaHoc.NgayPhatHanh,KHOAHOC.MaKhoaHoc,NGUOIDUNG.MaNguoiDung From NGUOIDUNG
 join GIOHANG
 on  GIOHANG.MaNguoiDung=NGUOIDUNG.MaNguoiDung
 join KHOAHOC on KHOAHOC.MaKhoaHoc= GIOHANG.MaKhoaHoc
